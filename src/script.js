6ba505e1e22c15b253ea30be89bf7e60239836db
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-03-11 23:51:25 -0300

    add api response

--- src/script.js
+++ src/script.js
@@ -1,16 +1,54 @@
 class EnglishDictionary {
   API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
   result = document.querySelector('#result');
-  sound = document.querySelector('#sound');
+  audio = document.querySelector('#audio');
+  audioBtn = document.querySelector('#audio-btn');
   search = document.querySelector('#search-btn');
+  inputWord = document.querySelector('#search-word');
 
   constructor() {
     this.search.addEventListener('click', () => {
-      let inputWord = document.querySelector('#search-word').value;
-      fetch(`${this.API_URL}${inputWord}`)
+      fetch(`${this.API_URL}${this.inputWord.value}`)
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+        this.displayResponse({
+          word: data[0].word,
+          pronunciation: data[0].phonetics[0].text,
+          category: data[0].meanings[0].partOfSpeech,
+          definition: data[0].meanings[0].definitions[0].definition,
+          example: data[0].meanings[0].definitions[0].example,
+          audio: data[0].phonetics[1].audio,
+        });
+      });
     });
+  }
+
+
+  displayResponse(data) {
+    console.log(data);
+    const elementMap = {
+      'word-title': data.word,
+      'pronunciation': data.pronunciation,
+      'category': data.category,
+      'definition': data.definition,
+      'example': data.example,
+    }
+
+    Object.entries(elementMap).forEach(([id, value]) => {
+      const element = document.getElementById(id);
+
+      if(value === undefined) {
+        element.classList.add('hide');
+        return;
+      }
+      
+      element.classList.remove('hide');
+      element.innerText = value;
+    });
+
+    this.audio.setAttribute('src', data.audio);
+    this.audioBtn.addEventListener('click', () => this.audio.play());
   }
 }
 
