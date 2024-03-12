6b8c903242a2a1532a129e9764622bc10b4c73e2
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-03-12 00:56:24 -0300

    add messages

--- src/script.js
+++ src/script.js
@@ -1,14 +1,22 @@
 class EnglishDictionary {
   API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-  result = document.querySelector('#result');
+
   audio = document.querySelector('#audio');
   audioBtn = document.querySelector('#audio-btn');
+
   search = document.querySelector('#search-btn');
   inputWord = document.querySelector('#search-word');
+  result = document.querySelector('#result');
+  message = document.querySelector('#message');
 
   constructor() {
-    this.search.addEventListener('click', () => {
-      fetch(`${this.API_URL}${this.inputWord.value}`)
+    this.search.addEventListener('click', () => this.searchWord(this.inputWord.value));
+  }
+
+  searchWord(word) {
+    this.displayMessage(`Searching the meaning of "<strong>${word}</strong>"`); 
+    
+    fetch(`${this.API_URL}${word}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -19,11 +27,10 @@ class EnglishDictionary {
           definition: data[0].meanings[0].definitions[0].definition,
           example: data[0].meanings[0].definitions[0].example,
           audio: data[0].phonetics[1].audio,
-        });
-      });
-    });
+        })
+      })
+      .catch(this.displayMessage('Could not find the word'));
   }
-
 
   displayResponse(data) {
     console.log(data);
@@ -49,6 +56,12 @@ class EnglishDictionary {
 
     this.audio.setAttribute('src', data.audio);
     this.audioBtn.addEventListener('click', () => this.audio.play());
+    this.message.classList.add('hide');
+    this.result.classList.remove('hide');
+  }
+
+  displayMessage(text){
+    this.message.innerHTML = text;
   }
 }
 
